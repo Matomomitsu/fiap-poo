79afecd1bd615997c86a30c8a51d7b80d8c55b25
Mateus Tomomitsu <matomomitsu@gmail.com>
2026-03-08 17:40:49 -0300

    add Carro class

--- FiapRide/src/br/com/fiapride/model/Carro.java
+++ FiapRide/src/br/com/fiapride/model/Carro.java
@@ -1,29 +1,55 @@
 package br.com.fiapride.model;
 
-enum MarchaCarro{
-	P,
-	N,
-	D,
-	R
-}
+import br.com.fiapride.enums.MarchaCarro;
 
 public class Carro {
-	public String marca;
-	public double velocidade;
-	public double rpm;
-	public String cor;
-	public boolean isFreioMaoAcionado;
-	public MarchaCarro marcha;
-	
-	public Carro(String marca, String cor) 
-	{
-		this.marca = marca;
-		this.cor = cor;
-		this.velocidade = 0.0;
-		this.rpm = 0.0;
-		this.isFreioMaoAcionado = false;
-		this.marcha = MarchaCarro.P;
-	}
-	
-	public Andar()
+    public String marca;
+    public String cor;
+    public double velocidade;
+    public boolean freioAcionado;
+    public MarchaCarro marcha;
+
+    public Carro(String marca, String cor) {
+        this.marca = marca;
+        this.cor = cor;
+        this.velocidade = 0.0;
+        this.freioAcionado = false;
+        this.marcha = MarchaCarro.P;
+    }
+
+    public void acelerar(double velocidade) {
+        if (this.freioAcionado) {
+            System.out.println("Freio acionado. Não é possível acelerar.");
+            return;
+        }
+
+        if (this.marcha == MarchaCarro.P || this.marcha == MarchaCarro.N) {
+            System.out.println("Carro está na marcha " + this.marcha + " e não pode acelerar.");
+            return;
+        }
+
+        this.velocidade += velocidade;
+        System.out.println("Carro acelerando. Velocidade atual: " + this.velocidade + " km/h");
+    }
+
+    public void frear() {
+        if (this.freioAcionado) {
+            System.out.println("Freio já acionado");
+            return;
+        }
+
+        this.freioAcionado = true;
+        this.velocidade = 0.0;
+        System.out.println("Freiando carro. Velocidade atual: " + this.velocidade + " km/h");
+    }
+
+    public void mudarMarcha(MarchaCarro novaMarcha) {
+        if (this.marcha == novaMarcha) {
+            System.out.println("Carro já está na marcha " + this.marcha);
+            return;
+        }
+
+        this.marcha = novaMarcha;
+        System.out.println("Marcha alterada para: " + this.marcha);
+    }
 }
